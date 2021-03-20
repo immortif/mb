507fe5faa12f8b2ced89babaabcfd99cac9594f1
immortif <mbc1987@126.com>
2021-03-20 12:43:07 +0800

    Add test of original jdbc

--- src/test/java/ecom/mbc/AppTest.java
+++ src/test/java/ecom/mbc/AppTest.java
@@ -2,19 +2,69 @@ package ecom.mbc;
 
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        jdbcTest();
+    }
+
+    public void jdbcTest() {
+        Connection connection = null;
+        PreparedStatement prepareStatement = null;
+        ResultSet rs = null;
+
+        try {
+            // 加载驱动
+            Class.forName("com.mysql.jdbc.Driver");
+            // 获取连接
+            String url = "jdbc:mysql://127.0.0.1:3306/ssmdemo";
+            String user = "root";
+            String password = "870219";
+            connection = DriverManager.getConnection(url, user, password);
+            // 获取statement，preparedStatement
+            String sql = "select * from tb_user where id=?";
+            prepareStatement = connection.prepareStatement(sql);
+            // 设置参数
+            prepareStatement.setLong(1, 2l);
+            // 执行查询
+            rs = prepareStatement.executeQuery();
+            // 处理结果集
+            while (rs.next()) {
+                System.out.println(rs.getString("userName"));
+                System.out.println(rs.getString("name"));
+                System.out.println(rs.getInt("age"));
+                System.out.println(rs.getDate("birthday"));
+            }
+        } catch (Exception e) {
+            // TODO: Something
+        } finally {
+            try {
+                // 关闭连接，释放资源
+                if (rs != null) {
+                    rs.close();
+                }
+                if (prepareStatement != null) {
+                    prepareStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
     }
 }
